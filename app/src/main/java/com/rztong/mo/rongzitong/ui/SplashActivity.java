20c71bc426baa69f3ff0a7cb37a92655118f6fd3
Royal <royal@RoyaldeMacBook-Pro.local>
2017-05-12 19:08:52 +0800

    一些图片资源

--- app/src/main/java/com/rztong/mo/rongzitong/ui/SplashActivity.java
+++ app/src/main/java/com/rztong/mo/rongzitong/ui/SplashActivity.java
@@ -7,12 +7,11 @@ import android.view.View;
 
 import com.rztong.mo.rongzitong.R;
 import com.rztong.mo.rongzitong.base.BaseActivity;
-import com.rztong.mo.rongzitong.bean.User;
 import com.rztong.mo.rongzitong.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends BaseActivity {
 
-    private User user;
+
     private ActivitySplashBinding binding;
 
     @Override
@@ -20,23 +19,11 @@ public class SplashActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_splash);
         binding = DataBindingUtil.setContentView(this , R.layout.activity_splash);
-        getUserInfo();
+
     }
 
-    public void getUserInfo() {
-        user = new User();
-        user.setId("11111");
-        user.setName("guiyang");
-        user.setPhone("18393234234");
 
-        setData();
-    }
-
-    private void setData() {
-        binding.setUser(user);
-    }
-
-    public void btnClick(View v){
-        binding.tv.setText("qwer");
+    private void sendAdInfoReq(){
+        
     }
 }
